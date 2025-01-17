79624f24e0c8f289dc3b5617c61c25906058e489
JessiP23 <jessi316866@gmail.com>
2025-01-17 13:18:40 -0500

    git ignore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My app is from Jessi
         </a>
       </header>
     </div>
