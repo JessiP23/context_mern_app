2520d161a58cefed75f91684b049d64c9cb5cc5e
JessiP23 <jessi316866@gmail.com>
2025-01-17 21:49:20 -0500

    get users data from request body

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,3 +1,15 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+
+import { User } from '../models/Course';
+
+const router = express.Router();
+
+
+// register upcoming users
+router.post('/register', async (req, res) => {
+
+    // users data from request body
+    const {username, email, password} = req.body;
+});
