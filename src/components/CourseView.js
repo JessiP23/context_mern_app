8fdde9702050c0c7da1da1701efb67dd0897d7c8
JessiP23 <jessi316866@gmail.com>
2025-01-19 06:22:11 -0500

    naviation

--- src/components/CourseView.js
+++ src/components/CourseView.js
@@ -54,9 +54,9 @@ const CourseView = () => {
 
     if (loading) {
         return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-black"></div>
-        </div>
+            <div className="flex items-center justify-center min-h-screen">
+                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-black"></div>
+            </div>
         );
     }
 
